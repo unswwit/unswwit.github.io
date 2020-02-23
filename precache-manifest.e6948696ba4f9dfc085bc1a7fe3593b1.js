7a79f2dd20acf77574ccbe5564ccacf7332b114e
Elicia Au Duong <eliciaauduong@outlook.com>
2020-02-23 19:02:22 +1100

    revert

---
+++ precache-manifest.e6948696ba4f9dfc085bc1a7fe3593b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eebbef55aab6b711c6c82f96d8463c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ad0327cf4088a0fe61",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d71a3ca96be9407c64c7",
+    "url": "/static/css/main.3dc0c08c.chunk.css"
+  },
+  {
+    "revision": "08ad0327cf4088a0fe61",
+    "url": "/static/js/2.99119dcb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.99119dcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d71a3ca96be9407c64c7",
+    "url": "/static/js/main.48efb5b8.chunk.js"
+  },
+  {
+    "revision": "1fffb0087cfde3306502",
+    "url": "/static/js/runtime-main.46917a9e.js"
+  }
+]);
