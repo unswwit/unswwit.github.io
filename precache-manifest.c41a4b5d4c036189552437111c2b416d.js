941c2b540ed80ee28ab2293fd7fbd5b785cb4d39
penny <penny@longs-mbp.home>
2020-05-20 18:43:29 +1000

    fix the routing in blogpost

---
+++ precache-manifest.c41a4b5d4c036189552437111c2b416d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e21ee73d0f1e4c88951ca8434ec010f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e36bbffb7e6ce508d99",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d654780b2dae19711496",
+    "url": "/static/css/main.8b62bb9f.chunk.css"
+  },
+  {
+    "revision": "5e36bbffb7e6ce508d99",
+    "url": "/static/js/2.c1db6301.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c1db6301.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d654780b2dae19711496",
+    "url": "/static/js/main.a79188ba.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
