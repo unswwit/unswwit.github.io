89666a165b075a94dcfea7b7d13d2abb7d982a2a
Elicia Au Duong <eliciaauduong@outlook.com>
2020-06-09 19:17:03 +1000

    fixed viv w fb link

---
+++ precache-manifest.b48fb28301b77f6a70869865c2d32233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c7a28c37263719512e3932ec07f7af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef28ed61d60a1f8ebc1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5a3fc76aa9194247eccf",
+    "url": "/static/css/main.db7aa410.chunk.css"
+  },
+  {
+    "revision": "5ef28ed61d60a1f8ebc1",
+    "url": "/static/js/2.e4261a4c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e4261a4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a3fc76aa9194247eccf",
+    "url": "/static/js/main.9e1d4873.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
