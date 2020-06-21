95bca839a6a487b8eb18bb028240d048a84bd18e
Elicia Au Duong <eliciaauduong@outlook.com>
2020-06-21 12:34:57 +1000

    blog post 7 release

---
+++ precache-manifest.f9035f379d3910ba3f541bb16998add6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86951e965c9d02ef115ed638b7076824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1947466a63b2649559ab",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4c3ee26d8cce22c7b1ce",
+    "url": "/static/css/main.fad3f1e0.chunk.css"
+  },
+  {
+    "revision": "1947466a63b2649559ab",
+    "url": "/static/js/2.4e195a90.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4e195a90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c3ee26d8cce22c7b1ce",
+    "url": "/static/js/main.521854d2.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
