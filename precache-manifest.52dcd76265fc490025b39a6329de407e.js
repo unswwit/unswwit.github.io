cff147d703524f409ebb5be9f8e69f668aad9942
Elicia Au Duong <eliciaauduong@outlook.com>
2020-07-31 13:01:15 +1000

    fixed publications spacing issue

---
+++ precache-manifest.52dcd76265fc490025b39a6329de407e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d33ea8f3de37358e26575025609cddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05279b28de290ec7add",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "636f6eb4c9dad1a26da5",
+    "url": "/static/css/main.388f95a4.chunk.css"
+  },
+  {
+    "revision": "c05279b28de290ec7add",
+    "url": "/static/js/2.96634c42.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.96634c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "636f6eb4c9dad1a26da5",
+    "url": "/static/js/main.3c748a2a.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
