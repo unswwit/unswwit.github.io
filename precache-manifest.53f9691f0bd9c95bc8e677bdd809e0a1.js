b7963fcae6046b2c19b3993631f5e406c6a23029
aaalana <41637549+aaalana@users.noreply.github.com>
2020-09-20 18:20:40 +1000

    capital letter blurb blog 24 fix

---
+++ precache-manifest.53f9691f0bd9c95bc8e677bdd809e0a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd91439a4973a5dee5060c47b1513285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280154a3197b56146c38",
+    "url": "/static/css/2.8364f30b.chunk.css"
+  },
+  {
+    "revision": "4b09db7a5311f7ae0fb0",
+    "url": "/static/css/main.65295fe0.chunk.css"
+  },
+  {
+    "revision": "280154a3197b56146c38",
+    "url": "/static/js/2.c6a8ab33.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.c6a8ab33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b09db7a5311f7ae0fb0",
+    "url": "/static/js/main.1e983256.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
