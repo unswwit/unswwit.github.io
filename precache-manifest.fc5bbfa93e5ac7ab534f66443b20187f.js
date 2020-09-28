cdc1c1e7e91e1ab0367553bbef7b0a9b07f489e1
aaalana <41637549+aaalana@users.noreply.github.com>
2020-09-28 14:20:38 +1000

    mobile bug fix

---
+++ precache-manifest.fc5bbfa93e5ac7ab534f66443b20187f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5515feeaa8543d81c1dc7edd38460f8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64b5c1d88813c9826e9",
+    "url": "/static/css/2.8364f30b.chunk.css"
+  },
+  {
+    "revision": "e55f7d659a858d8a0f88",
+    "url": "/static/css/main.679cb873.chunk.css"
+  },
+  {
+    "revision": "d64b5c1d88813c9826e9",
+    "url": "/static/js/2.fef5d6b0.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.fef5d6b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e55f7d659a858d8a0f88",
+    "url": "/static/js/main.9958c5fb.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
