d7ea51bc9a1ecff45d0536295f9824aead4e11c6
chelffey <chaffeychelsea@gmail.com>
2020-10-04 23:36:11 +1100

    update to header images for team and sponsors to reduce tint

---
+++ precache-manifest.9a598e1c3f7a0d8a949879f8d7799a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cc0258d987770413af25d896ecc1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64b5c1d88813c9826e9",
+    "url": "/static/css/2.8364f30b.chunk.css"
+  },
+  {
+    "revision": "b72cdc36e28ba48a0092",
+    "url": "/static/css/main.6821694a.chunk.css"
+  },
+  {
+    "revision": "d64b5c1d88813c9826e9",
+    "url": "/static/js/2.fef5d6b0.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.fef5d6b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b72cdc36e28ba48a0092",
+    "url": "/static/js/main.2e5cfeb9.chunk.js"
+  },
+  {
+    "revision": "3bcb6824372899d729c8",
+    "url": "/static/js/runtime-main.f32eaec2.js"
+  }
+]);
