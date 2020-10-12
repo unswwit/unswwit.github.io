f11861bf094e4c32723ee4ef256b0f0359f45a6f
aaalana <41637549+aaalana@users.noreply.github.com>
2020-10-13 09:58:07 +1100

    blog and events page fix

---
+++ precache-manifest.a1fbf5ed81cf08b5b57519600adff6bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e872a70f808e40af441b26e1f4085b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b1892d73ad10a383b3",
+    "url": "/static/css/2.aa13a6bf.chunk.css"
+  },
+  {
+    "revision": "74f8d09180cf36f67a4a",
+    "url": "/static/css/main.ee31e67c.chunk.css"
+  },
+  {
+    "revision": "54b1892d73ad10a383b3",
+    "url": "/static/js/2.58e8f8c9.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.58e8f8c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f8d09180cf36f67a4a",
+    "url": "/static/js/main.3b140174.chunk.js"
+  },
+  {
+    "revision": "3e60011f19d0b2029963",
+    "url": "/static/js/runtime-main.0625ef8c.js"
+  }
+]);
