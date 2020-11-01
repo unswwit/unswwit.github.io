d2c7fa984d5c734b42855f05eb3154c14938d08a
aaalana <41637549+aaalana@users.noreply.github.com>
2020-11-01 16:46:26 +1100

    blog 36 release

---
+++ precache-manifest.c8810e2c7266bc5d5baa608159fd96e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f948f75afc4c577ed1888523f93f2a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/css/2.aa13a6bf.chunk.css"
+  },
+  {
+    "revision": "b38b18c858e8518e349c",
+    "url": "/static/css/main.ed0d0b95.chunk.css"
+  },
+  {
+    "revision": "933c8afa2e2eba2ed661",
+    "url": "/static/js/2.ebe184be.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ebe184be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38b18c858e8518e349c",
+    "url": "/static/js/main.e5e0351c.chunk.js"
+  },
+  {
+    "revision": "3e60011f19d0b2029963",
+    "url": "/static/js/runtime-main.0625ef8c.js"
+  }
+]);
