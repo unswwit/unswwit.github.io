8e057d3fc0335b8924114c74c56c9250ff441214
aaalana <41637549+aaalana@users.noreply.github.com>
2020-12-14 21:40:44 +1100

    adding wit magazine

---
+++ precache-manifest.ac756724651c399d4e2281f085d6c293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f495a28cf8c4470a58b7a783ad4bf328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41323220547f3d4e27cd",
+    "url": "/static/css/2.aa13a6bf.chunk.css"
+  },
+  {
+    "revision": "acdbfdd4ea2196122f49",
+    "url": "/static/css/main.f44a2fc8.chunk.css"
+  },
+  {
+    "revision": "41323220547f3d4e27cd",
+    "url": "/static/js/2.9ee17d70.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.9ee17d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdbfdd4ea2196122f49",
+    "url": "/static/js/main.8691b363.chunk.js"
+  },
+  {
+    "revision": "3e60011f19d0b2029963",
+    "url": "/static/js/runtime-main.0625ef8c.js"
+  }
+]);
